ba6f9e20d219411b42dbca14a41791a0fcbbbd57
zxc20833zxc <zxc20833@gmail.com>
2025-08-30 17:22:25 +0000

    PORT CONFIG

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+   server: { // 👈 加上這個 server 物件
+    port: 8964 // 👈 指定你想要的 port，例如 3000
+  }
 })
